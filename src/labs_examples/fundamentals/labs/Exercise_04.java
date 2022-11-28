993a9bba476c328c3c3df2cd92e3eab4507d70ce
Thomas Hill <thomashill@thomass-air.home>
2022-11-28 15:05:14 +0000

    completed fundamentals labs

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -17,6 +17,29 @@ public class Exercise_04 {
 
         // write your code below
 
+        // declaring the variables
+
+        boolean isBool = true;
+        byte b = 5;
+        char c = 'a';
+        short s = 2022;
+        long l = 1000000l;
+        float f = 3.141459f;
+        double d = 3.1415926536;
+
+        // printing the variables
+
+        System.out.println("isBool a boolean = " + isBool);
+        System.out.println("a byte is small like b = " + b);
+        System.out.println("a char is a single character like c = " + c);
+        System.out.println("a short is a bit bigger than a byte and about the right size for storing a year like s = " + s);
+        System.out.println("a long can store biggggg numbers like l " + l);
+        System.out.println("a float stores decimals like pi = " + f);
+        System.out.println("but pi to more precision needs a double like d = " + d);
+
+
+
+
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -15,14 +15,35 @@ public class Exercise_05 {
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
 
+        int len = str.length();
+
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
 
+        boolean areStringsEqual = str.equals(str2);
+
         // please concatenate str & str2 and set the result to a new String variable below
+
+        String concatStrings = str + str2;
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
 
+        String str3 = "eat";
+
+        String str4 = "meat";
+
+        Boolean subString = str4.substring(1).equals(str3);
+
+        // printing results
+
+        System.out.println("str = " + str);
+        System.out.println("str2 = " + str2);
+        System.out.println("areStringsEqual = " +areStringsEqual);
+        System.out.println("str and str2 concatenated = " + concatStrings);
+        System.out.println("str3 = " + str3);
+        System.out.println("str4 = " + str4);
+        System.out.println("is str3 a substring of str4 = " + subString);
     }
 
 
