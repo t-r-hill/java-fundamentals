35ef43af3d87c8b9ccaad46b73e40e477c4798d5
Thomas Hill <tomhill555@gmail.com>
2022-12-06 18:21:12 +0000

    partially completed input output labs

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -11,8 +13,19 @@ package labs_examples.input_output.labs;
 
 class Example {
     public static void main(String[] args) {
+        String filePathIn = "src/labs_examples/input_output/files/example_text.txt";
+        String filePathOut = "src/labs_examples/input_output/files/example_text_copy.txt";
 
-
-
+        try(BufferedInputStream in = new BufferedInputStream(
+                new FileInputStream(filePathIn), 5);
+            FileOutputStream out = new FileOutputStream(filePathOut)){
+            int i = 5;
+            byte[] b = new byte[5];
+            while ((i = in.read(b)) != -1){
+                out.write(b);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -11,4 +13,53 @@ package labs_examples.input_output.labs;
  *      print out the unencrypted version. Does it match the original file?
  *
  */
+class Exercise_02{
+
+    public static void main(String[] args) {
+        fileEncrypter();
+        fileDecrypter();
+    }
+
+    public static void fileEncrypter(){
+        String filePathIn = "src/labs_examples/input_output/files/example_text.txt";
+        String filePathOut = "src/labs_examples/input_output/files/example_text_encrypted.txt";
+
+        try(BufferedReader in = new BufferedReader(new FileReader(filePathIn));
+            FileWriter out = new FileWriter(filePathOut)){
+            int i;
+            while((i = in.read()) != -1){
+                if(i == 'a'){
+                    out.write('-');
+                } else if (i == 'e') {
+                    out.write('*');
+                } else if (i == 'o') {
+                    out.write('^');
+                } else out.write((char) i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void fileDecrypter(){
+        String filePathIn = "src/labs_examples/input_output/files/example_text_encrypted.txt";
+        String filePathOut = "src/labs_examples/input_output/files/example_text_decrypted.txt";
+
+        try(BufferedReader in = new BufferedReader(new FileReader(filePathIn));
+            FileWriter out = new FileWriter(filePathOut)){
+            int i;
+            while((i = in.read()) != -1){
+                if(i == '-'){
+                    out.write('a');
+                } else if (i == '*') {
+                    out.write('e');
+                } else if (i == '^') {
+                    out.write('o');
+                } else out.write((char) i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 
