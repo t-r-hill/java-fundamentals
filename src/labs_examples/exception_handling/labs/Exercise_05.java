246de1db6eff306c4f296dfd345255278e946971
Thomas Hill <thomashill@thomass-air.home>
2022-12-06 09:17:38 +0000

    completed remaining exception handling labs

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -12,7 +12,13 @@ import java.util.Arrays;
 class Example {
 
     public static void main(String[] args) {
-
+        int[] nums = {9,7,5,3,1};
+        for (int num : nums){
+            System.out.println(num);
+            if (num == 3){
+                throw new ArithmeticException();
+            }
+        }
     }
 
 }
--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,29 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class Exercise_06{
+
+    public static void main(String[] args) {
+        ExceptionExample.catchesException();
+    }
+}
+
+class ExceptionExample{
+    public static void throwsException() throws ArithmeticException{
+        int[] nums = {9,7,5,3,1};
+        for (int num : nums){
+            System.out.println(num);
+            if (num == 3){
+                throw new ArithmeticException();
+            }
+        }
+    }
+
+    public static void catchesException(){
+        try{
+            throwsException();
+        } catch (ArithmeticException e){
+            System.out.println(e.toString());
+        }
+    }
+}
