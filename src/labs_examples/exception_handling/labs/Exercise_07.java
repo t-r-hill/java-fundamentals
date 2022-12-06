dc8ab6e61b99e9b09381a71cec944d1d9f27be52
Thomas Hill <thomashill@thomass-air.home>
2022-12-06 09:23:11 +0000

    completed remaining exception handling labs

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -6,3 +6,28 @@ package labs_examples.exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+class Exercise_07{
+    public static void main(String[] args){
+        try {
+            GenException.genException();
+        } catch (CustomException e) {
+            System.out.println(e.toString());;
+        }
+    }
+
+
+}
+
+class GenException{
+    public static void genException() throws CustomException {
+        throw new CustomException();
+    }
+}
+
+class CustomException extends Exception{
+    @Override
+    public String toString(){
+        return "Custom exception has been thrown";
+    }
+}
+
