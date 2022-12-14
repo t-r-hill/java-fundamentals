d7b8c25a411a88586b64d23797de12ca70fbc0f4
Thomas Hill <tomhill555@gmail.com>
2022-12-14 14:06:17 +0000

    partially completed lambdas labs

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -1,5 +1,8 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 /**
  * Lambdas Exercise 1:
  *
@@ -23,3 +26,71 @@ package labs_examples.lambdas.labs;
  *
  *
  */
+
+class Exercise_01{
+
+    public static void main(String[] args) {
+        // Part 1 + 2
+        NoParametersVoid anon1 = new NoParametersVoid() {
+            @Override
+            public void call() {
+                System.out.println("Called NoParametersVoid from anon");
+            }
+        };
+
+        NoParametersVoid lambda1 = () -> System.out.println("Called NoParametersVoid from lambda");
+
+        anon1.call();
+        lambda1.call();
+
+        // Part 3 + 4
+        OneParameterReturn<Integer> anon3 = new OneParameterReturn<Integer>() {
+            @Override
+            public Integer transform(Integer var) {
+                Integer a = 10 * var;
+                return a;
+            }
+        };
+
+        OneParameterReturn<Integer> lambda3 = (Integer x) -> x * 10;
+
+        System.out.println(anon3.transform(5));
+        System.out.println(lambda3.transform(5));
+
+        // Part 5 + 6
+        TwoParameterReturn<Integer, String> anon5 = new TwoParameterReturn<Integer, String>(){
+            @Override
+            public String BiTransform(Integer a, Integer b){
+                Integer c = a + b;
+                return "The sum of " + a + " and " + b + " equals " + c;
+            }
+        };
+
+        TwoParameterReturn<Integer, String> lambda5 = (Integer a, Integer b) -> {
+            Integer c = a + b;
+            return "The sum of " + a + " and " + b + " equals " + c;
+        };
+
+        System.out.println(anon5.BiTransform(2,3));
+        System.out.println(lambda5.BiTransform(2,3));
+
+        Function<Integer, Integer> lambda7 = (a) -> 4 * a +7;
+        Supplier<String> lambda8 = () -> "Supplied by get()";
+
+        System.out.println(lambda7.apply(6));
+        System.out.println(lambda8.get());
+    }
+}
+
+interface NoParametersVoid {
+    void call();
+}
+
+interface OneParameterReturn<T> {
+    T transform(T var);
+}
+
+interface TwoParameterReturn<T, R> {
+    R BiTransform(T var1, T var2);
+}
+
